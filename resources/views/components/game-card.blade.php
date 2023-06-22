de1b685fcdb0f3c781db6b8793544b9772f2b465
Hus47 <piebmarkuss@gmail.com>
2023-06-22 12:33:36 +0300

    game-card text size

--- resources/views/components/game-card.blade.php
+++ resources/views/components/game-card.blade.php
@@ -5,15 +5,15 @@
         <img src="https://picsum.photos/500/500" alt="Placeholder Image" class="object-cover rounded-t-lg">
     </div>
     <div class="flex justify-between absolute bottom-0 left-0 w-full bg-black bg-opacity-80 p-4 rounded-lg group-hover:bg-blue-950 group-hover:bg-opacity-100 transition-colors duration-300">
-        <h3 class="text-xl text-white">{{ $game->name }}</h3>
+        <h3 class="text-l text-white">{{ $game->name }}</h3>
         @if ($showPrice)
-            <div class="price">
+            <div class="price ml-1">
                 @if ($game->discount == 0)
                     <p class="text-white">€{{ $game->price / 100 }}</p>
                 @else
+                    <p class="text-green-600">-{{ $game->discount }}%</p>
                     <p class="text-white text-xs line-through">€{{ $game->price / 100 }}</p>
                     <p class="text-white">€{{ $game->price / 100 }}</p>
-                    <p class="text-green-600">-{{ $game->discount }}%</p>
                 @endif
             </div>
         @endif
